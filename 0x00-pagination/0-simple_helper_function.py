a40c62ab6d9e951ec9b5b251fd1938375141a922
kennywindis <kennyphilip100@gmail.com>
2023-05-12 08:12:40 -0700

    p

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Simple helper function """
+
+
+def index_range(page, page_size):
+    """ A function named index_range that takes two integer arguments page and page_size. """
+    if page and page_size:
+        start_index = (page - 1) * page_size
+        end_index = start_index + page_size
+        return start_index, end_index
---
+++ 0x00-pagination/README.md
@@ -0,0 +1,9 @@
+this is pagination
+
+Resources
+Read or watch:
+
+REST API Design: Pagination
+HATEOAS
+
+
