5db376472cd4d0188c6c5cadcddb1406f998e990
kennywindis <kennyphilip100@gmail.com>
2023-05-13 02:10:44 -0700

    p

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,9 +2,10 @@
 """ Simple helper function """
 
 def index_range(page, page_size):
-    """ A function named index_range that takes two integer arguments page and page_size. """
+    """ return a tuple of size two containing a start index and an end index
+        corresponding to the range of indexes to return in a list for those
+        particular pagination parameters. """
     if page and page_size:
         start_index = (page - 1) * page_size
         end_index = start_index + page_size
         return start_index, end_index
-
